47f15b1b3006a36e00974942276be386e1f7bced
Gabriel Ford <gabe@fordltc.net>
2024-09-23 18:50:24 -0400

    Refactoring

--- learn_ogl.cpp
+++ learn_ogl.cpp
@@ -5,6 +5,7 @@
 #include <glad/glad.h>
 
 #include <GLFW/glfw3.h>
+#include <tuple>
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -12,6 +13,7 @@ std::string getShaderFromFile(std::string file_path);
 bool checkForShaderCompileErrors(unsigned int shader);
 bool checkForProgramLinkingErrors(unsigned int program);
 void glfwInitialize();
+std::tuple<unsigned int, unsigned int> createShaderProgram();
 
 int main() {
   glfwInitialize();
@@ -34,80 +36,8 @@ int main() {
 
   glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
-  /* Shader Start */
-
-  /** Vertex Shader **/
-  auto vertexShaderSource = getShaderFromFile("./vert.glsl");
-  auto c_vertexShaderSource = vertexShaderSource.c_str();
-
-  float vertices[] = {
-      0.5f,  0.5f,  0.0f, // Top Right
-      0.5f,  -0.5f, 0.0f, // Bottom Right
-      -0.5f, -0.5f, 0.0f, // Bottom Left
-      -0.5f, 0.5f,  0.0f, // Top Left
-  };
-  unsigned int indices[] = {
-      0, 1, 3, // First Triangle
-      1, 2, 3  // Second Triangle
-  };
-
-  unsigned int EBO;
-  glGenBuffers(1, &EBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-               GL_STATIC_DRAW);
-
-  unsigned int VAO;
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  unsigned int vertexShader;
-  vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-  glShaderSource(vertexShader, 1, &c_vertexShaderSource, NULL);
-  glCompileShader(vertexShader);
-
-  if (!checkForShaderCompileErrors(vertexShader)) {
-    return 1;
-  }
-
-  /** Fragment Shader **/
-  auto fragmentShaderSource = getShaderFromFile("./frag.glsl");
-  auto c_fragmentShaderSource = fragmentShaderSource.c_str();
-
-  unsigned int fragmentShader;
-  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &c_fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
-
-  if (!checkForShaderCompileErrors(fragmentShader)) {
-    return 1;
-  }
-
-  /** Shader Program **/
-  unsigned int shaderProgram;
-  shaderProgram = glCreateProgram();
-
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
-
-  glDeleteShader(vertexShader);   // No longer needed after linking
-  glDeleteShader(fragmentShader); // No longer needed after linking
-
-  if (!checkForProgramLinkingErrors(shaderProgram)) {
-    return 1;
-  }
-
-  /* Shader End */
+  unsigned int shaderProgram, EBO;
+  std::tie(shaderProgram, EBO) = createShaderProgram();
 
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
@@ -183,4 +113,79 @@ void glfwInitialize() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+std::tuple<unsigned int, unsigned int> createShaderProgram() {
+  /** Vertex Shader **/
+  auto vertexShaderSource = getShaderFromFile("./vert.glsl");
+  auto c_vertexShaderSource = vertexShaderSource.c_str();
+
+  float vertices[] = {
+      0.5f,  0.5f,  0.0f, // Top Right
+      0.5f,  -0.5f, 0.0f, // Bottom Right
+      -0.5f, -0.5f, 0.0f, // Bottom Left
+      -0.5f, 0.5f,  0.0f, // Top Left
+  };
+  unsigned int indices[] = {
+      0, 1, 3, // First Triangle
+      1, 2, 3  // Second Triangle
+  };
+
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+  glShaderSource(vertexShader, 1, &c_vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  if (!checkForShaderCompileErrors(vertexShader)) {
+    return std::make_tuple(0, 0);
+  }
+
+  /** Fragment Shader **/
+  auto fragmentShaderSource = getShaderFromFile("./frag.glsl");
+  auto c_fragmentShaderSource = fragmentShaderSource.c_str();
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &c_fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  if (!checkForShaderCompileErrors(fragmentShader)) {
+    return std::make_tuple(0,0);
+  }
+
+  /** Shader Program **/
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);   // No longer needed after linking
+  glDeleteShader(fragmentShader); // No longer needed after linking
+
+  if (!checkForProgramLinkingErrors(shaderProgram)) {
+    return std::make_tuple(0,0);
+  }
+
+  return std::make_tuple(shaderProgram, EBO);
 }
